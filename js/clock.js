32ae4a7e6d332fd8fcbd0827f2369e5eef95bf62
SharifH <sharifhadidi@gmail.com>
2014-01-23 16:55:37 -0800

    Cleaning up setIntervals

--- js/clock.js
+++ js/clock.js
@@ -1,42 +1,43 @@
-$(function(){
-
-  setInterval( function() {
+$(document).ready(function() {
+  setInterval(function() {
     var currentDate = new Date();
-
-    var hours = currentDate.getHours();
-    var mins = currentDate.getMinutes();
-    var secs = currentDate.getSeconds();
-
-    var hdegree = hours * 30 + (mins / 2);
-    var mdegree = mins * 6;
-    var sdegree = secs * 6;
-
-    $(".hand").css("opacity","1");
-
-    updateRotation("hour", hdegree);
-    updateRotation("minute", mdegree);
-    updateRotation("second", sdegree);
-  }, 1000 );
-
-  var updateRotation = function (id, degrees) {
-    var rotation = "rotate(" + degrees + "deg)";
-
-    $("#" + id).css({
-        "-webkit-transform" : rotation,
-        "-moz-transform" : rotation,
-        "-ms-transform" : rotation,
-        "-o-transform" : rotation,
-        "transform" : rotation
-      })
-  };
-
+    updateAnalogClock(currentDate);
+    updateDigitalClock(currentDate);
+  }, 1000);
 });
-function updateClock(){
 
-  var currentTime = new Date ();
-  var currentHr = currentTime.getHours();
-  var currentMin = currentTime.getMinutes();
-  var currentSecs = currentTime.getSeconds();
+function updateRotation(id, degrees) {
+  var rotation = "rotate(" + degrees + "deg)";
+
+  $("#" + id).css({
+    "-webkit-transform" : rotation,
+    "-moz-transform" : rotation,
+    "-ms-transform" : rotation,
+    "-o-transform" : rotation,
+    "transform" : rotation
+  });
+}
+
+function updateAnalogClock(currentDate) {
+  var hours = currentDate.getHours();
+  var mins = currentDate.getMinutes();
+  var secs = currentDate.getSeconds();
+
+  var hdegree = hours * 30 + (mins / 2);
+  var mdegree = mins * 6;
+  var sdegree = secs * 6;
+
+  $(".hand").css("opacity","1");
+
+  updateRotation("hour", hdegree);
+  updateRotation("minute", mdegree);
+  updateRotation("second", sdegree);
+}
+
+function updateDigitalClock(currentDate) {
+  var currentHr = currentDate.getHours();
+  var currentMin = currentDate.getMinutes();
+  var currentSecs = currentDate.getSeconds();
 
   currentMin = (currentMin < 10 ? "0" : "") + currentMin;
   currentSecs = (currentSecs < 10 ? "0" : "") + currentSecs;
@@ -49,4 +50,3 @@ function updateClock(){
 
   document.getElementById("clock").firstChild.nodeValue = currentTimeString;
 }
-
